eeeee28ab2e3cfdda4a5985a9ad84d3ef5824196
AndreyBernardoni <andreybernardoni.gr041@academico.ifsul.edu.br>
2022-07-02 00:01:33 -0700

    implementado termo de notebooks

--- index.js
+++ index.js
@@ -1,21 +1,72 @@
-var pdf = require("html-pdf");
-var aejs = require("async-ejs");
+const express = require('express');
+const bodyParser = require('body-parser');
 
+// Instance of pdf generator
+const pdf = require("html-pdf");
 
-aejs.renderFile(
-  "./notebookTemplate.ejs",
-  { userName: name, machineNumber: machineNumber, setor: setor }, {async: true},
-  (err, html) => {
-    if (err) {
-      console.log(err);
-    } else {
-      pdf.create(html, {}).toFile("./output/teste.pdf", (err, res) => {
-        if (err) {
-          return console.log(err);
-        } else {
-          console.log(res);
+// Instance of ejs
+const ejs = require("async-ejs");
+
+const app = express();
+
+// Setting de configuration
+const PORT = process.env.PORT || 3000;
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Starting the server
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+// Route for the index page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Route for the notebook term form
+app.get('/notebook', (req, res) => {
+    res.render('notebook');
+});
+
+// Route for the success page
+app.get('/successful', (req, res) => {
+    res.render('successful');
+});
+
+// Route for generate pdf
+app.post('/generate', (req, res) => {
+
+    // Getting the current date
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const today = date.getDate();
+    const currentMonth = date.getMonth() + 1;
+    const currentDate = `${today}/${currentMonth}/${currentYear}`;
+
+    // Transforming the data into UpperCase
+    var machineId = req.body.machineId.toUpperCase();
+    var sector = req.body.sector.toUpperCase();
+
+    // Rendering the html template
+    ejs.renderFile(
+        "./templates/notebookTemplate.ejs", { userName: req.body.name, machineNumber: machineId, sector: sector, date: currentDate },
+        (err, html) => {
+            if (err) {
+                console.log(err);
+            } else {
+                // Generating the pdf
+                pdf.create(html, {}).toFile("./output/" + req.body.name + "_" + machineId + ".pdf", (err, res) => {
+                    if (err) {
+                        return console.log(err);
+                    } else {
+                        return console.log(res);
+                    }
+                });
+            }
         }
-      });
-    }
-  }
-);
+    );
+    // Redirecting to the success page
+    res.redirect('/successful');
+});
